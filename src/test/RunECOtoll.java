607f78d18144bf657e797802aaf4a56df789812f
salvasal <salvatoresalernitano96@gmail.com>
2019-03-31 16:49:55 +0200

    Creazione metodo main

---
+++ src/test/RunECOtoll.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package test;
+
+/**
+ * @author Salernitano Salvatore Matricola 242016
+ *
+ */
+public class RunECOtoll {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
